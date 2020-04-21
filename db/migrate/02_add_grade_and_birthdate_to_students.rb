ed61d5f3c69eecdcca3e40482cd0894074f85faf
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-21 21:49:09 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,5 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change 
-   add_column :grade, :birthdate, :string
+   add_column :students, :birthdate, :string
   end  
 end  
