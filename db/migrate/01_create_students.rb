ce43f2b209bcc99b7118a3fcc5b4cb2ea374d96d
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-21 21:38:07 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,10 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    create_table :artists do |t|
+    create_table :studentss do |t|
       t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
     end
   end
 end
